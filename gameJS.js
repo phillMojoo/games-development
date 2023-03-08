fe0fd9e5e94acf5960bbdcca170b80cacf4dd9a5
phillMojoo <tupomojoo@gamil.com>
2023-03-08 22:56:51 +0200

    newestchange

--- gameJS.js
+++ gameJS.js
@@ -1,11 +1,20 @@
 (function(){
 
     let loginScene=document.getElementById("#loginArea");
+    let userName=document.getElementById("#userName");
+    let password=document.getElementById("#password");
+    let loginButton=document.getElementById("#loginButton");
 
-
-    
 
     loginScene.style.display="inline";
+
+    loginButton.addEventListener("click", function(){
+     
+        loginScene.style.display="none";
+
+    })
+
+    
 
 
 })();

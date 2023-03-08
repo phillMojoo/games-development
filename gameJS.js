29d87d1aa602a766297bcf7d8303f7be4ce4f9db
phillMojoo <tupomojoo@gamil.com>
2023-03-08 23:42:05 +0200

    new material

--- gameJS.js
+++ gameJS.js
@@ -1,16 +1,20 @@
 (function(){
 
     let loginScene=document.getElementById("loginArea");
+    let gameScene=document.getElementById("gameScene");
     let userName=document.getElementById("userName");
     let password=document.getElementById("password");
     let loginButton=document.getElementById("loginButton");
+    
 
 
     loginScene.style.display="inline";
+    gameScene.style.display="none"
 
     loginButton.addEventListener("click", function(){
      
         loginScene.style.display="none";
+        gameScene.style.display="inline";
 
     })
 
